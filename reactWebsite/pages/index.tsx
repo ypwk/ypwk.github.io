277bfafa7cd5c5b26261db948b89a2cc4c2a62ec
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2024-09-05 00:24:57 -0400

    update

--- reactWebsite/pages/index.tsx
+++ reactWebsite/pages/index.tsx
@@ -11,25 +11,22 @@ export default function Page() {
         </div>
         <div className="one-half column project-title">
           <a href="https://www.wm.edu/" target="_blank">
-            <img className="img-fit" src="images/wam-banner.png" />
+            <img className="img-fit" src="images/uw-banner.png" />
           </a>
         </div>
       </div>
       <div className="row">
         <div className="one-half column">
           <p>
-            Hello there! My name is Kevin Wu, and I am an undergraduate student
-            at William & Mary graduating in 2024, majoring in Computer Science
-            and Mathematics.
+            Hello there! My name is Kevin Wu, and I am a doctoral student at the
+            University of Washington in the Electrical and Computer Engineering
+            department.
           </p>
           <p>
-            Right now, my interests lie in quantum computing, machine learning,
-            and optimization.
+            Right now, my interests lie in quantum computing and machine
+            learning.
           </p>
-          <p>
-            In my free time, I like to play the violin with my quartet and go to
-            the gym!
-          </p>
+          <p>In my free time, I like to play the violin and go to the gym!</p>
         </div>
         <div className="one-half column">
           <img className="img-fit" src="images/headshot.jpg" />
